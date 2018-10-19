8da204bb892ae1afe173965fc3d21d819fe919c5
https://github.com/RonaldMule/StoreManager <ronaldmulyowa@gmail.com>
2018-10-19 16:16:02 +0200

    fetch a single sales records[finishes #161285567]

--- end_points.py
+++ end_points.py
@@ -76,7 +76,29 @@ def get_sells():
     return jsonify({'sales': sales})
 
 
+@app.route('/api/v1.0/sales/<int:saleId>', methods = ['GET'])
+""" fetch a single sale record  """
+
+def get_task(task_id):
+    sale = [sold_item for sold_item in sales if sold_item['sellId'] == id]
+    return jsonify({'product_item': sale[0]})
+
+#Creating a sale order
+@app.route('/api/v1.0/sales', methods = ['POST'])
+def create_sale():
+    cart = {
+            'id': sales[-1]['id'] +1,
+            'name': request.json['name'],
+            'category':request.json['category'],
+            'price': request.json['price'],
+            'sold by': request.json['sold by'], 
+            'done': False
+    }  
+    sales.append(cart) 
+    return jsonify({'cart': cart}), 201 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+

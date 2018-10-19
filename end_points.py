50c8a6133974d77e74d28895387d091cc99dfd13
https://github.com/RonaldMule/StoreManager <ronaldmulyowa@gmail.com>
2018-10-19 15:51:42 +0200

    Allowing admin to post products[finishes #161285138]

--- end_points.py
+++ end_points.py
@@ -33,6 +33,20 @@ def get_product(productId):
   product = [product_item for product_item in products if product_item['id'] == productId ]
   return jsonify({'product_item': product[0]}) 
 
+@app.route('/api/v1/products', methods = ['POST'])
+def create_product():
+    """allowing admin to post products"""
+    product = {
+            'id': products[-1]['id'] +1,
+            'name': request.json['name'],
+            'quantity_avaliable':request.json['quantity_avaliable'],
+            'category':request.json['category'],
+            'price': request.json['price'],
+            'done': False
+    }  
+    products.append(product) 
+    return jsonify({'product': product}), 201 
+
 
 
 if __name__ == '__main__':
